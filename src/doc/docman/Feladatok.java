5ef5b1f08208fb39bb99d488d6ca506932d7466a
d0ci <no>
2020-03-19 14:09:50 +0100

    Finito

--- src/doc/docman/Feladatok.java
+++ src/doc/docman/Feladatok.java
@@ -1,7 +1,79 @@
 package doc.docman;
 
+import java.io.File;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Feladatok {
-    private ArrayList felfedezesek;
+    private ArrayList<Felfedezes> felfedezesek;
+    private String vegyjel;
+
+    //---------------
+    public Feladatok(){
+        this.Beolvasas();
+    }
+    //---------------
+    public void Beolvasas(){
+        try{
+            Scanner sc_felfedezes = new Scanner(new File("felfedezesek"));
+            this.felfedezesek = new ArrayList<>();
+            sc_felfedezes.nextLine();
+            while(sc_felfedezes.hasNext()){
+                felfedezesek.add(new Felfedezes(sc_felfedezes.nextLine()));
+            }
+        }catch (Exception e){
+            System.out.println(e.getMessage());
+        }
+    }
+    //---------------
+    public String harmadikFeladat(){
+        return String.format("3.feladat: Elemek száma: %d", this.felfedezesek.size());
+    }
+    //---------------
+    public String negyedikfeladat(){
+        int sumlalo = 0;
+        for (Felfedezes f : felfedezesek){
+            if (f.getEv().equals("Ókor")){
+                sumlalo++;
+            }
+        }
+        return "3.feladat: Felfedezések száma az ókorban: " + sumlalo;
+    }
+    //---------------
+    public void otodikFeladat(){
+        Scanner sc = new Scanner(System.in);
+        do {
+            System.out.print("5.feladat: Kérek egy vegyjelet: ");
+            vegyjel = sc.nextLine();
+        }while(!vegyjel.matches("[a-zA-Z]+") || (vegyjel.isEmpty() || vegyjel.length()>2));
+    }
+    //---------------
+    public String hatodikFeladat(){
+        for (Felfedezes f : felfedezesek){
+            if (f.getVegyjel().toLowerCase().equals(vegyjel.toLowerCase())){
+                return String.format("6.feladat: Keresés:\n\tAz elem vegyjele: %s\n\tAz elem neve: %s\n\tRendszáma: %d\n\tFelfedezés éve: %s\n\tFelfedező: %s",
+                        f.getVegyjel(),f.getNev(),f.getRendszam(),f.getEv(),f.getFelfedezo());
+            }
+        }
+        return "6.feladat: Keresés:\n\tNincs ilyen elem az adatbázisban!";
+    }
+    //---------------
+    public String hetedikFeladat(){
+        ArrayList<Felfedezes> xd = new ArrayList<>();
+        int dif;
+        for (Felfedezes f : felfedezesek){
+            if (!(f.getEv().equals("Ókor"))){
+                xd.add(f);
+            }
+        }
+        int index = xd.size()-1;
+        dif = Integer.parseInt(xd.get(index).getEv()) - Integer.parseInt(xd.get(index-1).getEv());
+        while (!(index == 1)){
+            index--;
+            if (dif < Integer.parseInt(xd.get(index).getEv()) - Integer.parseInt(xd.get(index-1).getEv())){
+                dif = Integer.parseInt(xd.get(index).getEv()) - Integer.parseInt(xd.get(index-1).getEv());
+            }
+        }
+        return String.format("7.feladat: %d év volt a leghosszabb időszak két elem felfedezése között.", dif);
+    }
 }
--- src/doc/docman/Main.java
+++ src/doc/docman/Main.java
@@ -1,8 +1,14 @@
 package doc.docman;
 
+
 public class Main {
 
     public static void main(String[] args) {
-
+        Feladatok f = new Feladatok();
+        System.out.println(f.harmadikFeladat());
+        System.out.println(f.negyedikfeladat());
+        f.otodikFeladat();
+        System.out.println(f.hatodikFeladat());
+        System.out.println(f.hetedikFeladat());
     }
 }
